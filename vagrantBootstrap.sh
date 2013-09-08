bbb31e9dd467f38a9d64d7bc8b2f680d90c9f992
Steve Donie <steve@donie.us>
2013-09-08 10:26:16 -0500

    a vagrant environment for testing in that is very similar to what is on Heroku

---
+++ vagrantBootstrap.sh
@@ -0,0 +1,15 @@
+﻿#!/usr/bin/env bash
+
+# need to get ruby 2, which is not on this base box.
+# start by installing RVM (Ruby Version Manager)
+curl -L https://get.rvm.io | bash -s stable --ruby
+
+# make rvm usable
+source /home/vagrant/.rvm/scripts/rvm
+
+#install ruby 2, make it the default
+rvm use 2.0.0 --default
+
+# do the stuff that needs to be done for the rails app to run the first time
+cd /vagrant
+bundle install
