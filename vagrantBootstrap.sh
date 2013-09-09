ca6fd7f49a272ef398e1e265e1f3d954aa0114a5
Steve Donie <steve@donie.us>
2013-09-08 19:02:01 -0500

    I now have a working vagrant setup to test with

--- vagrantBootstrap.sh
+++ vagrantBootstrap.sh
@@ -2,14 +2,33 @@
 
 # need to get ruby 2, which is not on this base box.
 # start by installing RVM (Ruby Version Manager)
+echo ---------- installing RVM
 curl -L https://get.rvm.io | bash -s stable --ruby
 
 # make rvm usable
-source /home/vagrant/.rvm/scripts/rvm
+echo ---------- running rvm init script
+source /usr/local/rvm/scripts/rvm
 
 #install ruby 2, make it the default
+echo ---------- use Ruby 2
 rvm use 2.0.0 --default
 
 # do the stuff that needs to be done for the rails app to run the first time
+echo ---------- bundle install
 cd /vagrant
 bundle install
+
+#ensure postgresql is set up properly
+echo ---------- sudo su
+sudo su
+echo ---------- su postgres
+su postgres
+echo ---------- create pg users
+createuser -s --username=postgres vagrant
+createuser -s --username=postgres RailsTimeTracker
+
+echo ---------- rake db tasks
+rake db:create:all
+rake db:migrate
+
+echo ----------- all done! Run bundle exec rails server to start the app
